9e2542175d08cbd0766cc87040efbe696dc1ef8a
Artem Ryndin <artemryndinjob@gmail.com>
2022-01-12 23:22:29 +0500

    Test fail

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -17,6 +17,6 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"));
+                .body("data", equalTo("some Testtext"));
     }
 }
